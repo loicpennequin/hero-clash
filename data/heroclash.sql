428ec4ef6b0b3b3856014f6e4d9aa7a31c0f74b1
Daria <lo.pennequin@gmail.com>
2017-07-22 16:02:57 +0200

    updated database file

--- data/heroclash.sql
+++ data/heroclash.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 21 Juillet 2017 à 21:22
+-- Généré le :  Sam 22 Juillet 2017 à 14:01
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -48,7 +48,9 @@ CREATE TABLE `classes` (
 --
 
 INSERT INTO `classes` (`id`, `name`, `portrait`, `basic`, `price`, `health`, `mana`, `atk`, `matk`, `def`, `mdef`) VALUES
-(1, 'Pyromancer', '/assets/img/pyromancer.png', 1, NULL, 150, 200, 20, 60, 20, 30);
+(1, 'Pyromancer', '/assets/img/pyromancer.png', 1, NULL, 150, 200, 20, 60, 20, 30),
+(2, 'Archer', '/assets/img/archer.png', 1, NULL, 180, 120, 50, 30, 20, 20),
+(3, 'Knight', '/assets/img/knight.png', 1, NULL, 270, 120, 40, 25, 45, 30);
 
 -- --------------------------------------------------------
 
@@ -85,7 +87,9 @@ CREATE TABLE `heroes` (
 --
 
 INSERT INTO `heroes` (`id`, `user_id`, `class_id`, `active`, `skill1`, `skill2`, `skill3`, `skill4`) VALUES
-(1, 1, 1, 0, NULL, NULL, NULL, NULL);
+(1, 1, 1, 0, NULL, NULL, NULL, NULL),
+(2, 1, 2, 0, NULL, NULL, NULL, NULL),
+(3, 1, 3, 0, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -105,7 +109,8 @@ CREATE TABLE `heroes_skills` (
 --
 
 INSERT INTO `heroes_skills` (`id`, `hero_id`, `skill_id`) VALUES
-(1, 1, 1);
+(1, 1, 1),
+(2, 2, 4);
 
 -- --------------------------------------------------------
 
@@ -137,7 +142,9 @@ CREATE TABLE `skills` (
 
 INSERT INTO `skills` (`id`, `class_id`, `basic`, `price`, `name`, `description`, `cost`, `attack`, `heal`, `effect`, `effect_id`, `power`, `cooldown`, `ratio`) VALUES
 (1, 1, 1, NULL, 'Fire Ball', 'Target : Enemy <br>\r\nEffect : Throws a fireball at target, ealing 30+(60% of Matk) damage', 30, 1, NULL, NULL, NULL, 30, 1, 0.6),
-(2, 1, 0, 200, 'Flame Ground', 'Target : all ennemies <br>\r\nEffect : burn the ground beneath all ennemies, setting them ablaze. Ennemies are dealt 20(+30%) damage each turn for three turns.', 45, 1, NULL, NULL, NULL, 20, 4, 0.3);
+(2, 1, 0, 200, 'Flame Ground', 'Target : all ennemies <br>\r\nEffect : burn the ground beneath all ennemies, setting them ablaze. Ennemies are dealt 20(+30%) damage each turn for three turns.', 45, 1, NULL, NULL, NULL, 20, 4, 0.3),
+(3, 1, 0, 200, 'Fire Wall', 'Target: self <br>\r\nEffect: Summon a wall of flames in front of you that lasts for 2 turns. Any enemy attacking you takes 40(+60%Matk) damage.', 40, 0, NULL, 1, NULL, 40, 5, 0.6),
+(4, 2, 1, NULL, 'Power Shot', 'Target: Enemy\r\nEffect: launches a powerful arrow at target, dealing 60+(30%Matk)damage and reducing their Def by 10 for 2 turns.', 40, 1, NULL, 1, NULL, 60, 5, 0.3);
 
 -- --------------------------------------------------------
 
@@ -151,15 +158,22 @@ CREATE TABLE `users` (
   `login` varchar(24) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `gold` int(11) NOT NULL DEFAULT '0'
+  `gold` int(11) NOT NULL DEFAULT '0',
+  `games` int(11) NOT NULL DEFAULT '0',
+  `wins` int(11) NOT NULL DEFAULT '0',
+  `losses` int(11) NOT NULL DEFAULT '0',
+  `elo` int(11) NOT NULL DEFAULT '1200',
+  `team_slot1` int(11) DEFAULT NULL,
+  `team_slot2` int(11) DEFAULT NULL,
+  `team_slot3` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `password`, `email`, `gold`) VALUES
-(1, 'Daria', '$2a$10$Bg4pjwLUyg5lL0OjI7XvgOCG9ivynv.4TApVtiJMKOnaeQhH6tgsm', 'test@test.com', 1000000);
+INSERT INTO `users` (`id`, `login`, `password`, `email`, `gold`, `games`, `wins`, `losses`, `elo`, `team_slot1`, `team_slot2`, `team_slot3`) VALUES
+(1, 'Daria', '$2a$10$hlUnuz8iHQcZcLPMwSFi2OSqH91iQTNCAvzpVyd1M0QVXoPqP/21S', 'test@test.test', 0, 0, 0, 0, 1200, 1, 2, 3);
 
 --
 -- Index pour les tables exportées
@@ -209,7 +223,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `effects`
 --
@@ -219,17 +233,17 @@ ALTER TABLE `effects`
 -- AUTO_INCREMENT pour la table `heroes`
 --
 ALTER TABLE `heroes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `heroes_skills`
 --
 ALTER TABLE `heroes_skills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `skills`
 --
 ALTER TABLE `skills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
